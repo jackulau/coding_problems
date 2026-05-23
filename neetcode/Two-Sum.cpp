78f93e10bee64d5f541410f412662b577b95e17c
Jack Lau <72348727+jackulau@users.noreply.github.com>
2026-05-23 03:29:33 -0500

    Solve: Two Sum [c++]

--- neetcode/Two-Sum.cpp
+++ neetcode/Two-Sum.cpp
@@ -2,16 +2,23 @@
 // Platform: neetcode
 // Difficulty: Easy
 // Language: c++
-// Synced: 2026-05-23T08:29:24.900Z
-            int complement = target - nums[i];
-        }
-            if (memo.find(complement) != memo.end()) {
-                vector<int> result;
-            }
-                result.push_back(memo[complement]);
-                result.push_back(i);
-                return result;
-            else {
-                memo[nums[i]] = i;
-            }
-        return {-1, -1};
+// Synced: 2026-05-23T08:29:33.313Z
+Code  |  C++Visualize codeShare Solutionclass Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> memo;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (memo.find(complement) != memo.end()) {
+                vector<int> result;
+                result.push_back(memo[complement]);
+                result.push_back(i);
+                return result;
+            }
+            else {
+                memo[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
